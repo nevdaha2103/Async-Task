36c09d1229f0c61b52d1af4a87ac06d3ba94975a
nevdaha2103 <vlabimir200628@gmail.com>
2024-12-23 23:01:30 +0200

    Create task 2.js

---
+++ Task 2/task 2.js
@@ -0,0 +1,39 @@
+function promiseMap(array, callback) {
+    const promises = array.map(item => callback(item)); 
+    return Promise.all(promises); 
+}
+
+async function demoFunc() {
+
+    const numbers = [1, 2, 3, 4, 5];
+
+    const promiseTriple = (num) => {
+        return new Promise((resolve) => {
+            setTimeout(() => resolve(num * 3), Math.random() * 1000); 
+        });
+    };
+
+    try {
+        const results1 = await promiseMap(numbers, promiseTriple);
+        console.log(results1); 
+    } catch (error) {
+        console.error('Error in case 1:', error); 
+    }
+    
+    const numbers2 = [10, 20, 30];
+
+    const promiseSquare = (num) => {
+        return new Promise((resolve) => {
+            setTimeout(() => resolve(num * num), Math.random() * 1000);
+        });
+    };
+
+    try {
+        const results2 = await promiseMap(numbers2, promiseSquare);
+        console.log(results2);
+    } catch (error) {
+        console.error('Error in case 2:', error);
+    }
+}
+
+demoFunc();
