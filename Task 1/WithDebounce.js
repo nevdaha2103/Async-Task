8b0190598fdaa1e6b41e6f78103d41745551ba01
nevdaha2103 <vlabimir200628@gmail.com>
2024-12-28 16:50:26 +0200

    Update WithDebounce.js

--- Task 1/WithDebounce.js
+++ Task 1/WithDebounce.js
@@ -1,34 +1,56 @@
-async function asyncFind(array, asyncCallback) {
-    for (let item of array) {
-        if (await asyncCallback(item)) {
-            return item; 
+function asyncFind(array, asyncCallback, finalCallback) {
+    let index = 0;
+
+    function iterate() {
+        if (index >= array.length) {
+            return finalCallback(null, undefined);
         }
+
+        asyncCallback(array[index], (err, result) => {
+            if (err) {
+                return finalCallback(err); 
+            }
+
+            if (result) {
+                return finalCallback(null, array[index]);
+            }
+
+            index++;
+            iterate(); 
+        });
     }
-    return undefined;
+
+    iterate(); 
 }
 
 function debounceAsync(asyncFn, delay) {
     let timeout;
-    return async function (...args) {
+    return function (...args) {
         if (timeout) clearTimeout(timeout);
-        return new Promise((resolve) => {
-            timeout = setTimeout(() => resolve(asyncFn(...args)), delay);
-        });
+        timeout = setTimeout(() => {
+            asyncFn(...args);
+        }, delay);
     };
 }
 
 const numbers = [1, 2, 3, 4, 5];
 
-async function isGreaterThanThree(number) {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve(number > 4);
-        }, 100); 
-    });
+function isGreaterThanThree(number, callback) {
+    setTimeout(() => {
+        callback(null, number > 4);
+    }, 100);
 }
 
 const debouncedIsGreaterThanThree = debounceAsync(isGreaterThanThree, 300);
 
-asyncFind(numbers, debouncedIsGreaterThanThree).then((result) => {
-    console.log('Debounced async find result:', result); 
-});
+asyncFind(
+    numbers,
+    debouncedIsGreaterThanThree,
+    (err, result) => {
+        if (err) {
+            console.error('Error:', err.message);
+        } else {
+            console.log('Debounced async find result:', result);
+        }
+    }
+);
